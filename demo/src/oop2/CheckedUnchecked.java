c26b98de79c69463c9d9cc2227231fb88fd547b6
srikanthpragada <srikanthpragada@gmail.com>
2025-03-19 17:56:32 +0530

    19-MAR

---
+++ demo/src/oop2/CheckedUnchecked.java
@@ -0,0 +1,18 @@
+package oop2;
+
+public class CheckedUnchecked {
+
+	public static void main(String[] args) {
+		 
+		int n = Integer.parseInt("123");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
